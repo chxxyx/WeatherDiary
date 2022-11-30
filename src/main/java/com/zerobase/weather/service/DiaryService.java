e4c3a9f05664e93342cc1a7088246fbc3160d550
chxxyx <codud0826@naver.com>
2022-11-30 22:14:04 +0900

    feat : logger 사용

--- src/main/java/com/zerobase/weather/service/DiaryService.java
+++ src/main/java/com/zerobase/weather/service/DiaryService.java
@@ -1,4 +1,5 @@
 package com.zerobase.weather.service;
+import com.zerobase.weather.WeatherApplication;
 import com.zerobase.weather.domain.DateWeather;
 import com.zerobase.weather.domain.Diary;
 import com.zerobase.weather.repository.DateWeatherRepository;
@@ -15,6 +16,8 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -27,6 +30,7 @@ public class DiaryService {
 
 	private final DiaryRepository diaryRepository;
 	private final DateWeatherRepository dateWeatherRepository;
+	private static final Logger logger = LoggerFactory.getLogger(WeatherApplication.class); // 프로젝트 전체에서 사용함
 
 	@Value("${openweathermap.key}")
 	private String apiKey;
@@ -49,6 +53,7 @@ public class DiaryService {
 	@Transactional(isolation = Isolation.SERIALIZABLE)
 	public void createDiary(LocalDate date, String text) {
 
+		logger.info("started to create diary");
 		// 날씨 데이터 가져오기 (API or DB에서 가져오기)
 		DateWeather dateWeather = getDateWeather(date); // 반복 되는 파싱 과정 생략 !
 
@@ -60,6 +65,8 @@ public class DiaryService {
 		nowDiary.setDate(date);
 
 		diaryRepository.save(nowDiary);
+		logger.info("end to create diary");
+
 
 	}
 	// 시간 마다 날씨 데이터 저장 함수를 위한 날씨 가져오는 함수
@@ -98,6 +105,8 @@ public class DiaryService {
 	// 조회 api
 	@Transactional(readOnly = true)
 	public List<Diary> readDiary(LocalDate date) {
+
+		logger.debug("read diary");
 		return diaryRepository.findAllByDate(date);
 	}
 
