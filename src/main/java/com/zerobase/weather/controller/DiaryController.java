33f986a2240a97c85b423b5e7314c6019425a697
chxxyx <codud0826@naver.com>
2022-11-30 23:21:30 +0900

    feat : API documentation (swagger 적용)

--- src/main/java/com/zerobase/weather/controller/DiaryController.java
+++ src/main/java/com/zerobase/weather/controller/DiaryController.java
@@ -2,6 +2,8 @@ package com.zerobase.weather.controller;
 
 import com.zerobase.weather.domain.Diary;
 import com.zerobase.weather.service.DiaryService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.Getter;
@@ -22,6 +24,7 @@ public class DiaryController {
 
 	private final DiaryService diaryService;
 
+	@ApiOperation(value = "일기 텍스트와 날씨를 이용해서 DB에 일기 저장합니다.", notes = "이건 노트 ") // 한줄 설명
 	@PostMapping ("/create/diary") // 요청을 보낼 때 넣어주는 파라미터(날짜를 url 뒤에 파라미터로 요청할 수 있음),(바디에 데이터 넣어서 전송)
 	void createDiary(@RequestParam @DateTimeFormat(iso= ISO.DATE) LocalDate date, @RequestBody String text) {
 		// 받은 값들을 서비스에 전달
@@ -30,20 +33,25 @@ public class DiaryController {
 	}
 
 	// 일기 조회
+	@ApiOperation(value = "선택한 날짜의 모든 일기 데이터를 가져옵니다.", notes = "이건 노트 ")
 	@GetMapping("/read/diary")
 	List<Diary> readDiary(@RequestParam  @DateTimeFormat(iso= ISO.DATE) LocalDate date) {
 		// 요청 파라미터로 넘어와야 하는 건 해당 날짜의 일기를 조회하는 것이므로 date 필요.
 		return diaryService.readDiary(date);
 	}
 	// 일기 조회 - 2. 조회 날짜 범위 지정하기
+	@ApiOperation(value = "선택한 기간 중의 모든 일기 데이터를 가져옵니다.", notes = "이건 노트 ")
 	@GetMapping("/read/diaries")
-	List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso= ISO.DATE) LocalDate startDate,
-							@RequestParam @DateTimeFormat(iso= ISO.DATE) LocalDate endDate) {
+	List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso= ISO.DATE)
+							@ApiParam(value = "조회할 기간의 첫번 째 날", example = "2022-11-30")LocalDate startDate,
+							@RequestParam @DateTimeFormat(iso= ISO.DATE)
+							@ApiParam(value = "조회할 기간의 마지막 날", example = "2022-11-30") LocalDate endDate) {
 		return  diaryService.readDiaries(startDate, endDate);
 
 	}
 
 	// 일기 수정
+	@ApiOperation(value = "입력 날짜에 따른 일기를 수정할 수 있습니다.", notes = "이건 노트 ")
 	@PutMapping("/update/diary") // 어떤 날짜의 일기를 수정할 것인지 ? (param) , 새로 작성할 값은 ? (RequestBody)
 	void updateDiary(@RequestParam @DateTimeFormat(iso= ISO.DATE) LocalDate date,
 						@RequestBody String text) {
@@ -52,6 +60,7 @@ public class DiaryController {
 	}
 
 	// 일기 삭제
+	@ApiOperation(value = "선택 날짜의 일기를 삭제합니다.", notes = "이건 노트 ")
 	@DeleteMapping("/delete/diary")
 	void deleteDiary(@RequestParam @DateTimeFormat(iso= ISO.DATE) LocalDate date) {
 
